0fb5ef9ca203764f45703d54c5499d865442d1a2
owinogabriel <g73346920@gmail.com>
2025-07-25 20:21:20 +0300

    chored:Modified.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-// src/App.tsx
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
-
-
 
 function App() {
   return (
